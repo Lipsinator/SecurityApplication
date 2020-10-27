c92125532d161a2ce9ba789ff89d7aa5ab9f5e2d
Christiaan Lips <Lipsy@localhost.localdomain>
2020-10-27 11:51:09 +0100

    starboard scanning with polaris fully scripted

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 #==============================================================
-# security script
+# simple security script
 #==============================================================
 # CHANGELOG
 #==============================================================
-# 21-10-2020 first check including selinux check enabled.
-#
+# 26-10-2020 - starboard aan de praat en polaris scan werkende.
+# 27-10-2020 - poaris scan schrijf logs nu naar eigen map en maakt file aan per deployment.
 #
 #
 #
@@ -13,44 +13,48 @@ clear
 echo -e "\e[32m================================="  
 echo -e "+ Starting security application +"
 echo -e "=================================\e[0m"
-sudo cp starboard /usr/local/bin
 echo -e "\e[31mPlease note that in order for this application to work you need to be logged in on your OpenShift platform and have root access\e[0m"
-read -p "Do you want to continue? [y/n]: " yesOrNoVar
 
+# place the starboard binary in $PATH
+sudo cp starboard /usr/local/bin
 
-if [ $yesOrNoVar = 'y' ]
-then
-	oc get namespaces | awk '{print $1, $8}' |  sed 1,1d > namespaceslist.txt
-	nameSpaceCount=$(wc -l namespaceslist.txt | cut -f1 -d' ')
+startStop="y"
+while [[ $startStop == "y" ]]; do
 	
-	echo -e "\e[32mYour current namespaces: \e[0m"	
+	# get and write all the namespaces running on the platform to screen
+	nameSpaces=`oc get namespaces | awk '{print $1, $8}' |  sed 1,1d`
 	
-	for ((i = 0; i < nameSpaceCount; i++ ));
-	do
-		awk '{if(NR=='$i') print $0}' namespaceslist.txt		
-	done
-
+	echo -e "\e[32mYour current namespaces: \e[0m"  
+	while read line; do echo "$line"; done <<< "$nameSpaces"	
+	
+	# select and switch to a chosen namespace.
 	read -p "Enter a namespace to see scannable deployments: " selectedNamespace
-	
 	oc project $selectedNamespace
 	clear
+
+	# list the deployments running in selected namespace and select one
 	echo -e "\e[32mYour current deployments in $selectedNamespace: \e[0m" 
 	oc get deployments | awk '{print $1, $8}' |  sed 1,1d
 
 	read -p "Enter a deployment to initaite a polaris scan: " selectedDeployment
 	
+	# initiate polaris scan over chosen depoyment and write the logs to a file in the polarislogs dir
 	starboard polaris deployment/$selectedDeployment --namespace $selectedNamespace & 
+	touch polarislogs/$selectedNamespace$selectedDeployment.txt
 	starboard get configaudit deployment/$selectedDeployment \
   	--namespace $selectedNamespace \
-  	--output yaml
+  	--output yaml > polarislogs/$selectedNamespace$selectedDeployment.txt
+	echo -e "\e[32mThe result of the scan has been saved in the polarislogs folder\e[0m"
 
-else	
-	clear
-	echo -e "\e[31m================================="  
-	echo -e "+ Thank you for using this app  +"
-	echo -e "=================================\e[0m"
+	# ask to start the sequence again
+	read -s -p "Do you want to scan another namespace? [y/n]: " startStop
+done
 
-fi
+clear
+
+echo -e "\e[31m================================="  
+echo -e "+ Thank you for using this app  +"
+echo -e "=================================\e[0m"
 
 
 
